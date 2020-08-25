3abdbec9ea0a396d1448d885879b0b5733f6e4c8
svetpru <sv.prud@gmail.com>
2020-08-25 13:56:16 +0300

    25.08.2020

--- 2 JS/2 JS/script.js
+++ 2 JS/2 JS/script.js
@@ -1,7 +1,32 @@
 
-
-function game (event){
-    console.log(event);
+function grow(element){
+    let step = 10;
+    let h = parseInt(   window.getComputedStyle(element)["height"].replace("px", "")  );
+    console.log(h+step);
+    element.style.height = h+step + "px";
 }
 
+
+function game (event){
+    console.log(event.keyCode);
+
+
+    if(event.keyCode == 32){
+        grow(p1);
+    }
+
+    else if(event.keyCode == 38){
+        grow(p2);
+    }
+
+    else{
+        console.log("Нажата неправильная клавиша")
+    }
+
+}
+
+
+
+
 document.addEventListener("keyup", game);
+
